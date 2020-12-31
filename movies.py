17a7ff746070404343b5c3dc6fc103dbf52057b4
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2020-12-31 09:11:36 -0500

    Add files via upload

--- movies.py
+++ movies.py
@@ -10,16 +10,19 @@ Test account:
 
 import requests
 import sys
-import argparse
+import re
 from bs4 import BeautifulSoup
 from pprint import pprint
 import json
+from time import time
 from time import sleep, time
 
-from rich.console import Console
-from rich import print as rprint
-from rich import pretty
+# =======================================================================================================================================================================
+# Global stuff (argparse stuff and rich console constructor).
+# CL args and the console object from rich should be globals.
+# Don't bother constructing console objects in every function.
 
+import argparse
 parser = argparse.ArgumentParser(description='letterboxd args')
 parser.add_argument('--user', '-u', dest='user', help='letterboxd.com user')
 parser.add_argument('--reviews', '-r', dest='reviews', action="store_true", default=False, help='Gets reviews')
@@ -30,45 +33,22 @@ parser.add_argument('--browser-open', '-b', dest='browserOpen', action="store_tr
 parser.add_argument('--search', '-s', nargs='+', dest='search', default=())
 args = parser.parse_args()
 
+
+from rich.console import Console
+from rich import print as rprint
 console = Console()
-pretty.install()
-
-def moviesOnSinglePage(url):
-    html_text = requests.get(url).text
-    soup = BeautifulSoup(html_text, 'html.parser')
-    divs = soup.find_all("div", {'class': "poster film-poster really-lazy-load"})
-    movieList = []
-    for div in divs:
-        findMovie = str(div).split('"')
-        movie = [i.split('/film/')[1].replace('/', '') for i in findMovie if i.startswith('/film/')]
-        movieList.append(movie[0])
-
-    return movieList
-
-def allUserMovies(user, base):
-    page = 1
-    totalMoviesList = []
-    while True:
-        url = f'{base}{page}'
-        movies = moviesOnSinglePage(url)
-        if len(movies) == 0:
-
-            break
-        for movie in movies:
-            totalMoviesList.append(movie)
-        page+=1
-
-    return totalMoviesList
-
+# =======================================================================================================================================================================
 
 # Make a list of the pages of reviews. (Ex. /user/films/reviews/page/1 ... /user/films/reviews/page/5)
-# Can use the previews of the reviews (before clicking more) to get review text for multiple movies.
-# Doing it like this is faster than requesting each review individually even with the string manipulation needed.
+# Can use the previews of the reviews (before clicking more) to get review text for multiple movies, if the review is short enough.
+# Doing it like this is faster than requesting each review individually .
+
 def getReviewUrls(user):
     reviewsBaseUrl = f'https://letterboxd.com/{user}/films/reviews/'
     html_text = requests.get(reviewsBaseUrl).text
     soup = BeautifulSoup(html_text, 'html.parser')
     pageDiv = str(soup.find("div", {'class': "pagination"}))
+    sleep(.05)
     try:
         lastValidPage = int(pageDiv.split('/films/reviews/page/')[-1][0])
         return [f'{reviewsBaseUrl}page/{str(i)}' for i in range(1, lastValidPage + 1)]
@@ -81,46 +61,82 @@ def getSingleReview(url=''):
     for possibleUrl in (url, url + '/1/'): # super-8 review had a an extra /1/ on the end
         soup = BeautifulSoup(requests.get(possibleUrl).text, 'html.parser')
         reviewDivHtmlStr = str(soup.find("div", {'class': "review body-text -prose -hero -loose"}))
+        sleep(.05)
         if not reviewDivHtmlStr  == 'None':
             return '<p>' + reviewDivHtmlStr.split('<div><p>')[-1].replace('</div>', '')
 
+
+
+# Should probably make different functions for batch getting all movies and searching.
+# Because args.search should only be checked once, not every time there's another movie review.
+# But it was too easy to just throw that in and add a continue
+
 def getReviews(user):
     movieDelim = f'[red]{"=" * 80}'
-    console.print(movieDelim)
+    look = f'/{user}/film/'
+    reviewsText = {}
+
+    if args.search:
+        console.print(movieDelim)
+        for url in [f'https://letterboxd.com/{user}/film/{movie}/' for movie in args.search]:
+            movie = url.split('/film/')[-1][:-1]
+            console.print(f'[cyan]movie: [bold blue]{movie}')
+            console.print(f'\t[green]Searching')
+            console.print(f'\t[green]Requesting: {url}')
+            console.print(movieDelim)
+
+            reviewsText[movie] = getSingleReview(url=url)
+        return reviewsText
+
+
 
     reviewUrls = getReviewUrls(user)
+    console.print('[cyan] Urls with multiple reviews')
+    rprint(reviewUrls)
+    print()
 
-    reviewsText = {}
+    console.print(movieDelim)
     for url in reviewUrls:
-        lines = requests.get(url).text.splitlines()
-        strippedLines = [line.strip() for line in lines]
+        console.print(f'[cyan]Requesting: [bold blue]{url}')
+        start = time()
+        response = requests.get(url)
+        rprint(f'reponseTime={time() - start}')
+        console.print(movieDelim)
+        htmlText = response.text
 
-        for line in strippedLines:
-            if line[0:24] == '<li class="film-detail">':
-                movie = line.split('data-film-slug="/film/')[1].split('/')[0]
+        valuableStart = htmlText.find('<ul class="poster-list -p70 film-list clear film-details-list no-title">')
+        valuableEnd = htmlText.find('</section>', valuableStart)
+        smallStr = htmlText[valuableStart:valuableEnd]
 
-                if args.search:
-                    if not movie in args.search:
-                        continue
+        for line in [line for line in smallStr.splitlines() if line.strip()[10:24] =='"film-detail">']:
 
-                console.print(f'[cyan]movie: [bold blue]{movie}')
-                reviewPreview = '<p>' + ''.join(line.split('<p>')[1:]).split(' </div>')[0]
 
-                #If the full review text is in the preview, don't bother requesting another page.
-                if not reviewPreview[-5] == '…':  #NOT THREE PERIODS - DIFFERENT UNICODE CHAR
-                    console.print('\t[blue]Preview contains full review')
-                    console.print('\t[blue]No need to request individual page')
-                    console.print(movieDelim)
+            shorterLine = line[line.find('<p>'):]
 
-                    reviewsText[movie] = reviewPreview
-                else:
-                    movieReviewUrl = f'https://letterboxd.com/{user}/film/{movie}/'
-                    console.print('\t[magenta]Preview contains partial review')
-                    console.print(f'\t[magenta]Requesting: {movieReviewUrl}')
-                    console.print(movieDelim)
+            movieSearch = re.search( fr'{look}.*?/', shorterLine).group()
+            movie = re.sub(fr'{look}','', str(movieSearch))[:-1]
 
-                    reviewsText[movie] = getSingleReview(url=movieReviewUrl)
-                    #sleep(.05)
+            console.print(f'[cyan]movie: [bold blue]{movie}')
+            reviewPreview = shorterLine[:shorterLine.find('</div>')].strip()
+
+            if '…' == reviewPreview[-5]: #NOT THREE PERIODS - DIFFERENT UNICODE CHAR
+                movieReviewUrl = f'https://letterboxd.com/{user}/film/{movie}/'
+                console.print('\t[magenta]Preview contains partial review')
+                console.print(f'\t[magenta]Requesting: {movieReviewUrl}')
+                console.print(movieDelim)
+
+                reviewsText[movie] = getSingleReview(url=movieReviewUrl)
+
+            else:
+                console.print('\t[blue]Preview contains full review')
+                console.print('\t[blue]No need to request individual page')
+                console.print(movieDelim)
+
+                reviewsText[movie] = reviewPreview
+
+
+
+            sleep(.05)
 
     return reviewsText
 
@@ -156,13 +172,12 @@ def writeReviews(reviewsDict={}):
         open_new_tab(fname)
 
 
-def letterboxdRun(args):
+def letterboxdRun():
     user = args.user
     baseUrl = f'https://letterboxd.com/{user}/films/'
 
     if args.reviews:
         fname = f'{user}_reviews.json'
-
         reviewsText = getReviews(user)
 
         outputDict = {'user': user, 'reviews': reviewsText}
@@ -172,26 +187,16 @@ def letterboxdRun(args):
 
         if args.json:
             rprint(f'json={fname}')
-            jsonStr = json.dumps(outputDict, indent = 3)
+            jsonStr = json.dumps(outputDict, indent=3)
             with open(fname, 'w+') as f:
                 f.write(jsonStr)
 
 
-
-def main():
-    #argsDict = vars(args)
+if __name__ == '__main__':
     console.print('[cyan]*Command line arguments* ')
-    #rprint(f'\t{args}')
-    #console.print('[blue]{')
     for k,v in vars(args).items():
         rprint(f'\t{k}={v}')
     print()
 
     console.print('[cyan]--Making requests to letterboxd.com--\n[red]This may take some time depending on how many reviews there are.\n')
-
-
-    letterboxdRun(args)
-
-
-if __name__ == '__main__':
-    main()
+    letterboxdRun()
